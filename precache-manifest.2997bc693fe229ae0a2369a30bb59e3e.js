7190fe884700d03c19409d4f47b507499391ec29
ushanagasri <ushanagasri2000@gmail.com>
2020-07-23 17:10:15 +0530

    Updates

---
+++ precache-manifest.2997bc693fe229ae0a2369a30bb59e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d206c9be027b4c32004bddf4a267e3",
+    "url": "/TaskManager/index.html"
+  },
+  {
+    "revision": "ff65374242073a14a7d3",
+    "url": "/TaskManager/static/css/main.7f200f35.chunk.css"
+  },
+  {
+    "revision": "6cb911ef59be5323e9d5",
+    "url": "/TaskManager/static/js/2.ca9b2b81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskManager/static/js/2.ca9b2b81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff65374242073a14a7d3",
+    "url": "/TaskManager/static/js/main.bb008d6b.chunk.js"
+  },
+  {
+    "revision": "393b9f22d8966d77f2a4",
+    "url": "/TaskManager/static/js/runtime-main.6de54237.js"
+  }
+]);
